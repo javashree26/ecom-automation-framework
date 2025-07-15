6f714be52a443d52c2f49a934460a223573da8e9
dhanashree <dhana@Dhanashree>
2025-07-15 18:50:21 +0530

    Added alert handling, positive login assertion, and logout check via POM

--- src/test/java/com/automation/base/BaseTest.java
+++ src/test/java/com/automation/base/BaseTest.java
@@ -19,6 +19,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest {
 
 	protected WebDriver driver;
+	// sample e-commerce site
+	private static final String appURL = "https://www.demoblaze.com";
 
 	@BeforeMethod
 	public void setUp() {
@@ -31,7 +33,7 @@ public class BaseTest {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 		// open the test site
-		driver.get("https://www.demoblaze.com"); // sample e-commerce site
+		driver.get(appURL);
 
 	}
 
--- src/test/java/com/automation/tests/LoginTest.java
+++ src/test/java/com/automation/tests/LoginTest.java
@@ -3,9 +3,17 @@
  */
 package com.automation.tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.automation.base.BaseTest;
+import com.automation.pages.HomePage;
 import com.automation.pages.LoginPage;
 
 /**
@@ -20,11 +28,25 @@ public class LoginTest extends BaseTest {
 		LoginPage loginPage = new LoginPage(driver);
 
 		// perform actual login
-		loginPage.login("testuser", "testpass");
+		loginPage.login("test", "test");
+
+		// verifying whether login is successful
+		HomePage homePage = new HomePage();
+		Assert.assertTrue(homePage.isLogoutButtonVisible(driver),
+				"Login might have failed — logout button not visible");
 		try {
-			Thread.sleep(3000); // just to observe, will remove later
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test(enabled = false)
+	public void testInvalidLogin() {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login("crash_1230", "crash_987");
+
+		String alertText = loginPage.getLoginAlertText();
+		Assert.assertEquals(alertText, "User does not exist.");
 	}
 }
